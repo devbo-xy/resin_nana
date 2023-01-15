d6fd6c68ef5e6b71c379d0a3c2ca7f11d4a8fe93
boxxy <91877601+devbo-xy@users.noreply.github.com>
2023-01-16 00:00:21 +0700

    tes

--- application/config/database.php
+++ application/config/database.php
@@ -75,7 +75,7 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
+	'hostname' => 'localhost:8080',
 	'username' => '',
 	'password' => '',
 	'database' => '',
