09b59d28451187c7e65c637db207e6b089d99832
alvinvin2001 <alvinjesus1@gmail.com>
2023-01-15 22:38:20 +0700

    check

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/resin_nana';
+$config['base_url'] = 'https://63c41d21010f20000830deb4--silly-semifreddo-cc23e0.netlify.app/';
 
 /*
 |--------------------------------------------------------------------------
